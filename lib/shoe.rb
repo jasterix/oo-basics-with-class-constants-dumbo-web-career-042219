39dfb114989ab64cecae5bc8cf3c91c0572e817a
Jasterix C <loune.calixte@gmail.com>
2019-04-24 17:16:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?brand != true
+    if BRANDS.include?brand != false
       BRANDS << brand
     end
   end
